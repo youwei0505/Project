e8f625d8acdc84ee27b23d660a3e4a3d74711bd8
Youwei Jiang <b053022059@g-mail.nsysu.edu.tw>
2020-09-10 13:22:58 +0800

    Project

---
+++ WebApplication1/WebApplication1/Controllers/ProjectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Projec
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
