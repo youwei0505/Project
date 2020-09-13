cdfad22f19da8577bcae3efdb04646c37d8a17ad
youwei_0505 <way271283@gmail.com>
2020-09-13 23:16:19 +0800

    new file

--- WebApplication1/WebApplication1/Scripts/script.js
+++ WebApplication1/WebApplication1/Scripts/script.js
@@ -1,14 +1,4 @@
 // 定義要取得的影音內容，包含影像和聲音
-let constraints = {
-    audio: true,
-    video: true
-}
+
 
 // 請求開啟影音裝置
-navigator.mediaDevices.getUserMedia(constraints)
-    .then(function (stream) {
-        // 取得當前裝置的影音串流（stream）
-    })
-    .catch(function (error) {
-        // 如果有錯誤發生
-    });
