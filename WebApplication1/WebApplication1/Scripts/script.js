978d91712a09c7ca69f93b4834668a6a02ba52f9
youwei_0505 <way271283@gmail.com>
2020-09-14 00:24:30 +0800

    move javascript to outside

--- WebApplication1/WebApplication1/Scripts/script.js
+++ WebApplication1/WebApplication1/Scripts/script.js
@@ -1,4 +1,171 @@
-// 定義要取得的影音內容，包含影像和聲音
+console.log("test")
+        navigator.getUserMedia || (navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia || navigator.msGetUserMedia);
+//navigator.mediaDevices.getUserMedia(myConstraints);
+if (!navigator.getUserMedia) {
+    alert("您的瀏覽器不支持");
+}
+
+var btn = document.getElementById('run');
+btn.onclick = startWebcam_front;
+
+var btn2 = document.getElementById('run2');
+btn2.onclick = startWebcam_back;
+
+var stop = document.getElementById('stop');
+stop.onclick = stopBothVideoAndAudio;
+
+//確認讀取的鏡頭畫面，取得權限
+//The video source is facing toward the user; this includes, for example, the front-facing camera on a smartphone.
+function startWebcam_front(e) {
+    navigator.getUserMedia({
+        video: { width: 480, height: 360 },
+        audio: false
+    }, onSuccess, onError);
+    //video 畫面 ， audio 影像
+    function onSuccess(stream) {
+
+        var video = document.getElementById('webcam');
+
+        if (window.URL) {
+            video.srcObject = stream;
+        } else {
+            video.src = stream;
+        }
+
+        video.autoplay = true;
+    }
+
+    function onError() { }
+}
+//This is the back camera on a smartphone.
+function startWebcam_back(e) {
+    navigator.getUserMedia({
+        video: { width: 480, height: 360 },
+        audio: false
+    }, onSuccess, onError);
+    //video 畫面 ， audio 影像
+    function onSuccess(stream) {
+
+        var video = document.getElementById('webcam');
+
+        if (window.URL) {
+            video.srcObject = stream;
+        } else {
+            video.src = stream;
+        }
+
+        video.autoplay = true;
+        // Elements for taking the snapshot
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        // Trigger photo take
+        document.getElementById("snap").addEventListener("click", function () {
+            context.drawImage(video, 0, 0, 640, 480);
+        });
+    }
+
+    function onError() { }
+}
+
+var device_id;
+
+//https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+//https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/enumerateDevices
+//----------------------mutiple
+if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    console.log("enumerateDevices() not supported.");
+}
+// List cameras and microphones.
+navigator.mediaDevices.enumerateDevices()
+    .then(function (devices) {
+        devices.forEach(function (device) {
+            console.log(device.kind + ": " + device.label +
+                " id = " + device.deviceId);
+            if (device.kind == "videoinput") {
+                console.log("videoinput");
+                device_id = device.deviceId;
+                console.log("~device_id~ = ", device_id);
+            }
+        });
+    })
+    .catch(function (err) {
+        console.log(err.name + ": " + err.message);
+    });
+console.log(device_id);
+
+var btn3 = document.getElementById('run3');
+btn3.onclick = startWebcam_test;
+//
+function startWebcam_test(e) {
+    console.log("@@device_id = ", device_id);
+    navigator.getUserMedia({
+        video: { deviceId: device_id },
+        audio: false
+    }, onSuccess, onError);
+    //video 畫面 ， audio 影像
+    function onSuccess(stream) {
+
+        var video = document.getElementById('webcam');
+
+        if (window.URL) {
+            video.srcObject = stream;
+        } else {
+            video.src = stream;
+        }
+
+        video.autoplay = true;
+
+        // Elements for taking the snapshot
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+        // Trigger photo take
+        document.getElementById("snap").addEventListener("click", function () {
+            context.drawImage(video, 0, 0, 640, 480);
+        });
+    }
+
+    function onError() { }
+}
+
+//stop
+function stopBothVideoAndAudio(stream) {
+    const tracks = mediaStream.getTracks()
+    tracks[0].stop();
+}
+
+    
+    
+//</script >
+//@* <body>
+//    <h2>緊急通報系統</h2>
+//    <div>
+//        <h4>畫面來源</h4>
+//        <video></video>
+
+//        <button id=" button1-1 onclick=" myFunction();">按鈕</button>
+//    <button id=" button1-2 onclick=" myFunction();">放大</button>
+//    <button id=" button1-3 onclick=" myFunction(); ">擷取</button>
 
 
-// 請求開啟影音裝置
+//        < video ></video >
+//            <button id=" button2 onclick=" myFunction(); ">按鈕</button>
+//                < hr />
+//                <h4>畫面來源</h4>
+//                <video src="http://127.0.0.1:12345/video_feed"></video>
+//                <button id=" button3 onclick=" myFunction(); ">按鈕</button>
+
+
+//                    < video ></video >
+//                        <button id=" button4" onclick="myFunction();">按鈕</button>
+//                        <hr />
+//    </div >
+
+
+
+
+//    <script>
+//        function myFunction() {
+//            var r = confirm("--是否確定要刪除?!--");
+//            console.log("click");
+//        }
+//    </script> *@
